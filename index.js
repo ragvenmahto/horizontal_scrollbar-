a5f925c8823a75da5e4ed9c68bba57434183c762
Agrim Srivastava <agrim.sri4@gmail.com>
2025-12-24 14:07:09 +0530

    Fix json-server dependency

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-const jsonServer = require("json-server"); // importing json-server library
+import jsonServer from "json-server";
+import cors from "cors";
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
+const PORT = process.env.PORT || 3000;
+
+server.use(cors());
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(PORT, () => {
+  console.log(`JSON Server running on port ${PORT}`);
+});
